5ed3a204c2426e86950b3f7dac183accdaceb95c
Gerson Junior <ger_son_junior@live.com.pt>
2022-04-09 23:41:14 +0100

    add new hero, navbar, button and search

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Header from '../componentes/utils/Header'
+import Header from '../componentes/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
-import Hero from "../componentes/website/Hero";
+import Hero from "../componentes/Hero";
+import Pesquisa from "../componentes/Pesquisa";
+import Curso from "../componentes/Curso";
 import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <div className="spacing bg-slate-100 w-full h-screen">
-      
-      <Hero/>
-
-      <div className="flex flex-row mt-96 ml-20 mr-20">
-          <input type="text" id="curso" name="curso" className="border border-slate-600 rounded w-3/4 h-10 px-5 " placeholder="Curso"/>
-          <button type="button" className="border border-slate-600 rounded w-1/4 h-10 ml-5">Click Me!</button>
-      </div>
-    
-
-    </div>    
-
-  )
+  <>
+    <Hero/>
+    <Pesquisa/>
+    <Curso/>
+  </>
+  );
 }
