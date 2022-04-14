9d6fcd4f9925ba75d9d36d762bf1becebd0026bb
Gerson Junior <ger_son_junior@live.com.pt>
2022-04-14 18:26:58 +0100

    add subject componentes

--- components/Curso.jsx
+++ components/Curso.jsx
@@ -1,32 +1,80 @@
 import React from 'react'
-import { useState } from "react";
 
-const lei = [
-                [
-                    {
-                        "ects" : 5 ,
-                        "cadeira" : "Analise"
-                    }
-                ],
-                [
-                    {
-                        "ects" : 5 ,
-                        "cadeira" : "Fisica Moderna"
-                    }
-                ]
+const lei = [ 
+              [
+              {
+                "ects" : 5 ,
+                "nome" : "Álgebra Linear para Engenharia"
+              }, 
+              {
+                "ects" : 5 ,
+                "nome" : "Cálculo para Engenharia"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Laboratórios de Informática I"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Programação Funcional"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Tópicos de Matemática Discreta"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Opção UMinho"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Análise Matemática para Engenharia"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Elementos de Probabilidades e Teoria de Números"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Laboratórios de Informática II"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Lógica"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Programação Imperativa"
+              },
+              {
+                "ects" : 5 ,
+                "nome" : "Sistemas de Computação"
+              },
             ]
+          ]
 
 
 
-export default  function Curso() {
-  
-  const val = 0;
-
+export default  function Curso() {  
   return (
-    <div className="flex flex-row justify-center pt-10 sm:pt-20">
-        {lei[val].map((elem) => {
+    <div className="flex flex-row justify-start pt-6 spacing">
+        {lei.map((elem, key) => {
           return (
-           <div/>
+           <div key={key} className="flew flew-row justify-start pt-10">
+                <div className="font-medium text-gray-600">
+                  1ºano
+                </div>
+                {elem.map((cadeira, i) => {
+                    return (
+                      <a key={i}>
+                        <div className="font-medium text-gray-600">
+                            {cadeira.nome}
+                        </div>
+                        <input type="text" id={cadeira.nome} name="cadeira" className="border border-slate-600 rounded w-1/4 h-6 px-5 " placeholder="Nota"/>
+                      </a>
+                    );
+                  })}
+           </div>
           );
         })}
     </div>
